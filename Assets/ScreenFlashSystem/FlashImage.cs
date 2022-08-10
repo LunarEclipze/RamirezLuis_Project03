52ea8e2b6ab0c7f14e6a542826bd1196710c9c49
Luis <LR4118@gmail.com>
2022-08-09 22:55:45 -0500

    Adds Screen Flash System
    Adding screen flash system

---
+++ Assets/ScreenFlashSystem/FlashImage.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class FlashImage : MonoBehaviour
+{
+    //This script contains the code to run your screen flash
+    //Attach this script to any blank canvas image
+
+    Image _image = null;
+    Coroutine _currentFlashRoutine = null;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void StartFlash(float secondsForOneFlash, float maxAlpha, Color newColor)
+    {
+        _image.color = newColor;
+
+        //Ensure Alpha isnt above 1
+        maxAlpha = Mathf.Clamp(maxAlpha, 0, 1);
+
+        if (_currentFlashRoutine != null)
+        {
+            StopCoroutine(_currentFlashRoutine);
+        }
+        _currentFlashRoutine = StartCoroutine(Flash(secondsForOneFlash, maxAlpha));
+    }
+
+    IEnumerator Flash(float secondsForOneFlash, float maxAlpha)
+    {
+
+        //Flash In
+        float flashInDuration = secondsForOneFlash / 2;
+        for (float t = 0; t < flashInDuration; t += Time.deltaTime)
+        {
+            Color colorThisFrame = _image.color;
+            colorThisFrame.a = Mathf.Lerp(0, maxAlpha, t / flashInDuration);
+            _image.color = colorThisFrame;
+
+            //Wait until next frame
+            yield return null;
+        }
+
+        //Flash Out
+        float flashOutDuration = secondsForOneFlash / 2;
+        for (float t = 0; t < flashOutDuration; t += Time.deltaTime)
+        {
+
+            Color colorThisFrame = _image.color;
+            colorThisFrame.a = Mathf.Lerp(maxAlpha, 0, t / flashOutDuration);
+            _image.color = colorThisFrame;
+
+            yield return null;
+
+        }
+
+        //Ensure maxAlpha is set to 0
+        _image.color = new Color32(0, 0, 0, 0);
+    }
+}
---
+++ Assets/ScreenFlashSystem/LevelController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    //This code is to test out the screen flash is working properly
+    //Add this script to an empty game object and hit space to test.
+
+    //Copy and paste this code on any object you wish to trigger a flash
+    [Header("Image Canvas")]
+    [SerializeField] FlashImage _flashImage = null;
+    [Header("Flash Settings")]
+    [SerializeField] Color _newColor = Color.red;
+    [SerializeField][Tooltip("How long your screen with flash")][Range(0,1)] float _secondsForFlash = 0f;
+    [SerializeField][Tooltip("The opacity of your flash")][Range(0,1)] float _maxAlpha = 0f;
+
+void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Copy and paste this code on any object you wish to trigger a flash
+            _flashImage.StartFlash(_secondsForFlash, _maxAlpha, _newColor);
+        }
+    }
+}
